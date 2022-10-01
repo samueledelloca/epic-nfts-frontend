76418cf84666be0056f5458e8df90db92f85a5f4
Samuele Dell'Oca <34474980+samueledelloca@users.noreply.github.com>
2022-10-01 10:16:54 +0000

    Basic functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
 import './styles/App.css';
-import twitterLogo from './assets/twitter-logo.svg';
+import githubLogo from './assets/github-logo-white.png';
+import { ethers } from "ethers";
+import myEpicNft from './utils/MyEpicNFT.json';
 
 // Constants
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const GITHUB_LINK = 'https://github.com/samueledelloca'
 const PIXXITI_LINK = '';
 const TOTAL_MINT_COUNT = 50;
+const CONTRACT_ADDRESS = "0xFEFeB4E05526a5Ab74bAfbe51D5B90E62b7dE28F";
 
 const App = () => {
 
@@ -22,10 +26,19 @@ const App = () => {
     const { ethereum } = window;
 
     if (!ethereum) {
-        console.log("Make sure you have metamask!");
-        return;
+      console.log("Make sure you have metamask!");
+      return;
     } else {
-        console.log("We have the ethereum object", ethereum);
+      console.log("We have the ethereum object", ethereum);
+      
+      let chainId = await ethereum.request({ method: 'eth_chainId' });
+      console.log("Connected to chain " + chainId);
+      
+      // String, hex code of the chainId of the Rinkebey test network
+      const goerliChainId = "0x5"; 
+      if (chainId !== goerliChainId) {
+      	alert("You are not connected to the Goerli Test Network!");
+      }
     }
 
     /*
@@ -40,6 +53,10 @@ const App = () => {
       const account = accounts[0];
       console.log("Found an authorized account:", account);
       setCurrentAccount(account);
+
+      // Setup listener! This is for the case where a user comes to our site
+          // and ALREADY had their wallet connected + authorized.
+          setupEventListener()
     } else {
       console.log("No authorized account found");
     }
@@ -58,8 +75,61 @@ const App = () => {
 
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]); 
+
+      // Setup listener! This is for the case where a user comes to our site
+      // and connected their wallet for the first time.
+      setupEventListener() 
     } catch (error) {
       console.log(error);
+    }
+  }
+
+  const setupEventListener = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+        
+        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber())
+          alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
+        });
+
+        console.log("Setup event listener!")
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const askContractToMintNft = async () => { 
+    try {
+      const { ethereum } = window;
+  
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+  
+        console.log("Going to pop wallet now to pay gas...")
+        let nftTxn = await connectedContract.makeAnEpicNFT();
+  
+        console.log("Mining...please wait.")
+        await nftTxn.wait();
+        
+        console.log(`Mined, see transaction: https://goerli.etherscan.io/tx/${nftTxn.hash}`);
+  
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
     }
   }
 
@@ -86,20 +156,20 @@ const App = () => {
             renderNotConnectedContainer()
            : 
             <>
-              <button onClick={null} className="cta-button connect-wallet-button">
+              <button onClick={askContractToMintNft} className="cta-button mint-button">
                 Mint NFT
               </button>
             </>
           }
         </div>
         <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+         <img alt="Twitter Logo" className="twitter-logo" src={githubLogo} />
           <a
             className="footer-text"
-            href={TWITTER_LINK}
+            href={GITHUB_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          >{` samueledelloca`}</a>
         </div>
       </div>
     </div>
